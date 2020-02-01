0f129e78667a1eab422984631444391f41e1766d
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-02-01 23:08:06 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,10 +54,5 @@ def self.find_by_name(name)
   DB[:conn].execute(sql, name)
 end
   
-  def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, name)
-end
-  
-  
+
 end
